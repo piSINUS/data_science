bdb73a2ac207fd794838edff85186c8c75fac1fc
piSINUS <104928495+piSINUS@users.noreply.github.com>
2024-08-15 22:35:12 +0300

    Add files via upload

--- algoritms.py
+++ algoritms.py
@@ -106,5 +106,25 @@ parents = {}
 graph["start"] ={}
 graph["start"]['a'] = 6
 graph['start']['b'] = 2
+graph['a'] = {}
+graph['a']['fin'] = 1
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+graph[ 'fin'] ={}
 
-print(graph['start'].keys())
+infinity = float('inf')
+
+costs = {}
+costs['a'] =6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['in'] = None
+
+processed = []
+
+# print(graph['start'].keys())
