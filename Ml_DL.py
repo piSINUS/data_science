df4fbc9e766c5e2e8df683f046ceeb08ab132c10
piSINUS <104928495+piSINUS@users.noreply.github.com>
2024-10-16 18:46:44 +0300

    Add files via upload

---
+++ Ml_DL.py
@@ -0,0 +1,24 @@
+import numpy as np
+class neuralNetwork:
+    def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+input_nodes = 3
+hidden_nodes =3
+output_nodes = 3
+
+learning_rate = 0.3
+
+n = neuralNetwork( input_nodes,hidden_nodes,output_nodes,learning_rate)
+s = np.random.rand(3,3)
+print(s)
